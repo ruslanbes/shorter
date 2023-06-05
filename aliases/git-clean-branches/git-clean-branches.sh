cff2d16e85dd6a6dd0e723a7b6c6b24ca29e4be7
Ruslan Bes <github@ruslanbes.com>
2023-06-05 19:56:24 +0200

    cleanup

--- aliases/git-clean-branches/git-clean-branches.sh
+++
@@ -1,4 +0,0 @@
-echo "Cleanup local orphaned branches"
-function git-clean-branches(){
-    git fetch -p && for branch in `git branch -vv | grep ': gone]' | awk '{print $1}'`; do git branch -D $branch; done
-}
